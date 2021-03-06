1d57374b24de10a041f6f22587af788505f392fa
이석원 <stevelee@gridwiz.com>
2021-03-07 01:14:27 +0900

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDnX0oHIxXxEYGKOzAhiNrx1Qxjo64NYlM',
+  authDomain: 'nwitter-e6528.firebaseapp.com',
+  projectId: 'nwitter-e6528',
+  storageBucket: 'nwitter-e6528.appspot.com',
+  messagingSenderId: '26890659964',
+  appId: '1:26890659964:web:6ce75172937be01c2ec1f9',
+};
+
+export default firebase.initializeApp(firebaseConfig);
