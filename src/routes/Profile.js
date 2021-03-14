324d6db2ab4c99144a6ad7df6a5b23455c869f47
이석원 <stevelee@gridwiz.com>
2021-03-15 00:34:49 +0900

    Update profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,39 @@
 import { authService } from 'fbase';
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      const response = await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={newDisplayName}
+          placeholder="Display name"
+          onChange={onChange}
+        />
+        <input type="submit" value="Update profile" />
+      </form>
       <button onClick={onLogOutClick}>Log out</button>
     </>
   );
