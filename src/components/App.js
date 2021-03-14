e328b6778fb788e1b754af5818f65529ef4e62ca
이석원 <stevelee@gridwiz.com>
2021-03-14 21:06:13 +0900

    input file & show preview

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,11 @@ import { authService } from 'fbase';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setLoggedIn] = useState(authService.currentUser);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setLoggedIn(true);
         setUserObj(user);
-      } else {
-        setLoggedIn(false);
       }
       setInit(true);
     });
@@ -20,7 +16,7 @@ function App() {
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         'Loading...'
       )}
